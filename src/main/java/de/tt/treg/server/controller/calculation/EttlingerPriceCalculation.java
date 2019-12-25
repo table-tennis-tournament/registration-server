3917baf7dcf4256210ec3c6dcc7bbdd9f50cf29b
Dane Leube <dane160290@gmail.com>
2019-12-25 13:29:40 +0100

    fix waiting list calculation bug

--- src/main/java/de/tt/treg/server/controller/calculation/EttlingerPriceCalculation.java
+++ src/main/java/de/tt/treg/server/controller/calculation/EttlingerPriceCalculation.java
@@ -10,37 +10,40 @@ import de.tt.treg.server.domain.PlayerCompetition;
 import de.tt.treg.server.service.impl.helper.CompetitionCache;
 
 public class EttlingerPriceCalculation implements ITournamentPriceCalculator {
-	
-	
-	public EttlingerPriceCalculation() {
-	}
 
-	@Override
-	public Player calculatePaymentAndPrepaymentForSinglePlayer(
-			Player player) {
-			double calculatedPrice = 0;
-			calculatedPrice = calculatePrice(player, calculatedPrice);
-			player.setPayment(calculatedPrice);
-			player.setPrepayment(calculatedPrice - (player.getCompetitions().size() * 2));
-		return player;
-	}
 
-	private double calculatePrice(Player player, double calculatedPrice) {
-		List<Integer> competitionIdList = getCompetitionIdList(player.getCompetitions());
-		for(Integer singleDisciplineId : competitionIdList){
-			Competition completeCompetition = CompetitionCache.getInstance().getCompetitionById(singleDisciplineId);
-			calculatedPrice+= completeCompetition.getPrice();
-		}
-		return calculatedPrice;
-	}
+    public EttlingerPriceCalculation() {
+    }
 
-	private List<Integer> getCompetitionIdList(
-			Set<PlayerCompetition> competitions) {
-		List<Integer> result = new ArrayList<Integer>();
-		for(PlayerCompetition competition : competitions){
-			result.add(competition.getPlayerCompetitionPk().getCompetitionId());
-		}
-		return result;
-	}
+    @Override
+    public Player calculatePaymentAndPrepaymentForSinglePlayer(
+            Player player) {
+        double calculatedPrice = 0;
+        calculatedPrice = calculatePrice(player, calculatedPrice);
+        player.setPayment(calculatedPrice);
+        player.setPrepayment(calculatedPrice - (player.getCompetitions().size() * 2));
+        return player;
+    }
+
+    private double calculatePrice(Player player, double calculatedPrice) {
+        List<Integer> competitionIdList = getCompetitionIdList(player.getCompetitions());
+        for (Integer singleDisciplineId : competitionIdList) {
+            Competition completeCompetition = CompetitionCache.getInstance().getCompetitionById(singleDisciplineId);
+            calculatedPrice += completeCompetition.getPrice();
+        }
+        return calculatedPrice;
+    }
+
+    private List<Integer> getCompetitionIdList(
+            Set<PlayerCompetition> competitions) {
+        List<Integer> result = new ArrayList<Integer>();
+        for (PlayerCompetition competition : competitions) {
+            if (competition.getIsWaitingList() != 0) {
+                continue;
+            }
+            result.add(competition.getPlayerCompetitionPk().getCompetitionId());
+        }
+        return result;
+    }
 
 }
